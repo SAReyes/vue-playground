2d3af71a12dfa3178e5e2d43f87b850ae2dab127
Adrian Reyes <sadrian.reyes@gmail.com>
2022-01-30 09:35:35 +0100

    feat: grid display

---
+++ css-layout/src/components/footer/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>Footer</footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+<style lang="scss" scoped>
+footer {
+  border: 4px solid cadetblue;
+}
+</style>
