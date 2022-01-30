988c33f492995dbadf3a9985ffd8fdead4b54b54
Adrian Reyes <sadrian.reyes@gmail.com>
2022-01-30 11:27:05 +0100

    feat: body title

--- css-layout/src/components/main/Main.vue
+++ css-layout/src/components/main/Main.vue
@@ -1,21 +1,40 @@
 <template>
-  <main>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <main class="main">
+    <div class="main__gradient" />
+    <h1 class="main__title">Nice pictures</h1>
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default defineComponent({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
 <style lang="scss" scoped>
-main {
-  background-color: $c-background-dark;
-  color: $c-text-dark;
+.main {
+  $title-height: rem(160);
+
+  position: relative;
+  padding: $spacing-s $spacing-m;
+
+  &__gradient {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -10;
+
+    height: $title-height;
+    background-color: $c-primary-accent;
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-image: url("~@/assets/gradient.png");
+  }
+
+  &__title {
+    height: calc(#{$title-height} - #{$spacing-s});
+    color: $c-text-dark;
+  }
 }
 </style>
