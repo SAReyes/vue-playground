ab6f2e72a1b169e795e76073aaf1bd83911488d9
Adrian Reyes <sadrian.reyes@gmail.com>
2022-01-30 10:18:46 +0100

    feat: header

--- css-layout/src/components/header/Header.vue
+++ css-layout/src/components/header/Header.vue
@@ -1,5 +1,11 @@
 <template>
-  <header>Header</header>
+  <header class="header">
+    <img class="header__logo" alt="acme corporation" src="@/assets/logo.png" />
+    <span class="header__user">
+      <span class="header__initials">JD</span>
+      <span>John Doe</span>
+    </span>
+  </header>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -7,7 +13,42 @@ import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
 <style lang="scss" scoped>
-header {
-  border: 4px solid red;
+.header {
+  $content-height: rem(48);
+
+  background-color: $c-background-light;
+  color: $c-text-light;
+  border-bottom: rem(5) solid $c-primary-accent;
+
+  padding: $spacing-s $spacing-m;
+
+  display: flex;
+  justify-content: space-between;
+
+  &__logo {
+    height: $content-height;
+  }
+
+  &__user {
+    display: flex;
+    gap: $spacing-s;
+    align-items: center;
+  }
+
+  &__initials {
+    background-color: $c-primary-accent;
+    color: $c-text-dark;
+
+    font-weight: 600;
+    font-size: rem(24);
+
+    height: $content-height;
+    width: $content-height;
+    border-radius: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
