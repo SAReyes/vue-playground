b4ad5b9ba0a946fc9c0fef3cac5cb4a167984778
Adrian Reyes <sadrian.reyes@gmail.com>
2022-01-30 13:43:58 +0100

    feat(css-layout): footer styles (#8)

--- css-layout/src/components/footer/Footer.vue
+++ css-layout/src/components/footer/Footer.vue
@@ -1,13 +1,26 @@
 <template>
-  <footer>Footer</footer>
+  <footer class="footer">
+    <span class="footer__users-number"> {{ users }} </span>&nbsp;active users
+  </footer>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    return {
+      users: Math.floor(Math.random() * 11),
+    };
+  },
+});
 </script>
 <style lang="scss" scoped>
-footer {
-  border: 4px solid cadetblue;
+.footer {
+  border-top: rem(5) solid $c-primary-accent;
+  padding: $spacing-s $spacing-m;
+
+  &__users-number {
+    color: $c-primary-accent;
+  }
 }
 </style>
