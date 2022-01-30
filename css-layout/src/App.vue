a66bc0d3afec9b31ee3f1c3e19c006d1160da064
Adrian Reyes <sadrian.reyes@gmail.com>
2022-01-30 10:11:46 +0100

    feat(css-layout): grid display (#4)

--- css-layout/src/App.vue
+++ css-layout/src/App.vue
@@ -1,27 +1,51 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Header />
+  <Main class="main" />
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "@/components/header/Header.vue";
+import Main from "@/components/main/Main.vue";
+import Footer from "@/components/footer/Footer.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    Footer,
+    Main,
+    Header,
   },
 });
 </script>
 
+<style lang="scss" scoped>
+.main {
+  overflow: auto;
+}
+</style>
+
 <style lang="scss">
+html,
+body {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   font-size: $font-size;
+
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+
+  overflow: hidden;
+  height: 100%;
 }
 </style>
---
+++ css-layout/src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>Header</header>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+<style lang="scss" scoped>
+header {
+  border: 4px solid red;
+}
+</style>
